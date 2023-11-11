bdafac02355c3e49230b5b01931ac2e49e6d0849
lon4ry <danyasafin@icloud.com>
2023-11-12 00:12:07 +0300

    going to a new repository

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,9 +1,9 @@
-import { NextFont } from 'next/dist/compiled/@next/font';
-import { IBM_Plex_Mono } from 'next/dist/compiled/@next/font/dist/google';
-import { Inter } from 'next/font/google';
-
-const inter: NextFont = Inter({ subsets: ['cyrillic'], weight: ['500'] });
-const ibmPlexMono: NextFont = IBM_Plex_Mono({
-  subsets: ['cyrillic'],
-  weight: ['500'],
-});
+// import { NextFont } from 'next/dist/compiled/@next/font';
+// import { IBM_Plex_Mono } from 'next/dist/compiled/@next/font/dist/google';
+// import { Inter } from 'next/font/google';
+//
+// const inter: NextFont = Inter({ subsets: ['cyrillic'], weight: ['500'] });
+// const ibmPlexMono: NextFont = IBM_Plex_Mono({
+//   subsets: ['cyrillic'],
+//   weight: ['500'],
+// });
--- src/ui/screens/dashboard/dashboard.screen.tsx
+++ src/ui/screens/dashboard/dashboard.screen.tsx
@@ -1,15 +1,15 @@
-import useUser from '@/ui/hooks/use-user.hook';
+// import useUser from '@/ui/hooks/use-user.hook';
 import SidebarComponent from '@/ui/components/sidebar/sidebar.component';
 
 export default function DashboardScreen() {
-  const { payload: user, isLoading: isUserLoading } = useUser();
+  // const { payload: user, isLoading: isUserLoading } = useUser();
   return (
     <>
       <SidebarComponent />
       <div
         className={'fixed right-0 top-0 w-[calc(100%-3rem)] h-full bg-gray-200'}
       >
-        <div className={'f'}></div>
+        <div className={''}></div>
       </div>
     </>
   );
