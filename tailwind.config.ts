46a095c1a208174fd11579ffe0dc41da81a1e2c9
lon4ry <danyasafin@icloud.com>
2023-12-05 00:25:36 +0300

    npm update and config changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,15 +5,7 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     './src/ui/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
+  theme: {},
   plugins: [],
 };
 export default config;
